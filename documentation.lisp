be48d02277832a1cd8cefbf1ba617b8c1c659635
Shinmera <shinmera@tymoon.eu>
2023-08-21 14:56:01 +0200

    Minor doc improvements

--- documentation.lisp
+++ documentation.lisp
@@ -9,19 +9,18 @@ See CONVEX-HULL")
   (function convex-hull
     "Extract a convex hull mesh from the given set of vertices.
 
-VERTICES should be a sequence of REALs which describe the point cloud
+VERTICES should be a sequence of FLOATs which describe the point cloud
 to extract a convex hull from. Each vertex should come in the form of
 consecutive X, Y, and Z coordinates. It is preferred that VERTICES be
-a (SIMPLE-ARRAY SINGLE-FLOAT). Otherwise, the array is copied and
-elements coerced to single-floats.
+a (SIMPLE-ARRAY SINGLE-FLOAT (*)) or (SIMPLE-ARRAY DOUBLE-FLOAT (*)).
 
 If VERTICES only contains 2 or fewer vertices, an error is signalled.
 If VERTICES describes a point cloud that doesn't have any volume, an
 error is also signalled.
 
 Returns two values:
-  1. A (SIMPLE-ARRAY SINGLE-FLOAT) of vertices that lie on the convex
-     hull, in the same format as the input.
+  1. A simple array of vertices that lie on the convex hull, in the
+     same format as the input, including matching element-type.
   2. A (SIMPLE-ARRAY (UNSIGNED-BYTE 32)) of vertex indices, every
      triplet of which describes a triangle face on the hull. The
      vertex index must be multiplied by 3 in order to get the index of
@@ -31,5 +30,9 @@ If a convex hull cannot be constructed exactly due to floating point
 imprecision, a warning of type EDGE-SOLVER-FAILED may be signalled. If
 EDGE-SOLVER-FAILED is signalled, the resulting convex hull may not be
 optimal.
+
+You may pass an optional EPS parameter (defaulting to 0.0001), which
+defines the minimal distance below which two vertices are considered
+the same and are merged together.
 
 See EDGE-SOLVER-FAILED"))
